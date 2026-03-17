b0385351df2a6cc161bbede32f67c4388af996d6
LuizSalvador-GIT <luizcesar.contisalvador@gmail.com>
2026-03-17 09:43:05 -0300

    Teste

---
+++ Variáveis/Aula 02 variáveis.py
@@ -0,0 +1,13 @@
+nome = input("Qual o seu nome?")
+print(F"Seja muito bem vindo{nome}")
+
+dia=input("Qual seu dia de nascimento")
+Mês=input("Qual seu mês de nascimento?")
+ano=input("Qual seu ano de nascimento?")
+
+print(f"você nasceu em:dia{dia}/{Mês}/{ano}")
+
+numero1=("digite um numero inteiro")
+numero2=("digite um numero inteiro")
+
+print(f"sua idade = (numero1 - numero2)")
