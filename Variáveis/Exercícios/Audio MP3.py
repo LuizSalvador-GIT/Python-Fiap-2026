c3553b6c8ab755002e391c2b96f25eec9bf920a0
LuizSalvador-GIT <luizcesar.contisalvador@gmail.com>
2026-04-09 10:07:43 -0300

    Exercico sobre numero maior que o outro

--- Variáveis/Exercícios/Audio MP3.py
+++ Variáveis/Exercícios/Audio MP3.py
@@ -1,5 +1,9 @@
 num=int(input("Digite um número:"))
-if num%2==0:
-    print("numero par.")
+num2 = int(input("Digite outro numero"))
+
+if num > num2:
+    print(f"o maior numero é {num}.")
+elif num2 > num :
+    print(f" o maior numero é {num2}.")
 else:
-    print("numero impar.")
+    print(f"Os dois numeros são iguais.")
